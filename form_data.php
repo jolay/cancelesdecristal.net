3557107fd4a378b6f74117700f92fcb944e3bad5
9098 <tavo@neyra.mx>
2015-07-15 20:05:23 -0500

    Formulario

--- form_data.php
+++ form_data.php
@@ -2,25 +2,25 @@
     if(isset($_POST['mail'])) {
           
         // EDIT THE 2 LINES BELOW AS REQUIRED
-        $email_to = "hello@themestreet.net";
-        $email_subject = "Contact from Berliner Template";
+        $email_to = "ventas@cancelesdecristal.net,tavo@neyra.mx,olay.jorge@neyra.mx";
+        $email_subject = "Contacto desde Landing Page";
          
         
         $name = $_POST['name']; // required
         $email_from = $_POST['mail']; // required
         $text = $_POST['message']; // required
          
-        $email_message = "Message from Website: \n\n";
+        $email_message = "Mensaje desde sitio Tu Espacio en Cristal: \n\n";
          
         function clean_string($string) {
           $bad = array("content-type","bcc:","to:","cc:","href");
           return str_replace($bad,"",$string);
         }
          
-        $email_message .= "Name: ".clean_string($name)."\n";
+        $email_message .= "Nombre: ".clean_string($name)."\n";
         $email_message .= "Email: ".clean_string($email_from)."\n";
-        $email_message .= "Subject: ".clean_string('Berliner - contact form')."\n";
-        $email_message .= "Message: ".clean_string($text)."\n";
+        $email_message .= "Asunto: ".clean_string('Contacto desde sitio web')."\n";
+        $email_message .= "Mensaje: ".clean_string($text)."\n";
          
          
     // create email headers
@@ -29,12 +29,12 @@
     'X-Mailer: PHP/' . phpversion();
     if(@mail($email_to, $email_subject, $email_message, $headers)) {
         $data['status'] = TRUE;
-        $data['msg'] = 'E-mail has been sent!';
-        $data['class'] = 'success';
+        $data['msg'] = 'Su mensaje ha sido enviado';
+        $data['class'] = 'Hecho';
     } else {
         $data['status'] = FALSE;
-        $data['msg'] = 'Your e-mail has not been sent!';
-        $data['class'] = 'error';
+        $data['msg'] = 'Número seleccionado incorrecto';
+        $data['class'] = 'Error';
     }
     
     echo json_encode($data);
